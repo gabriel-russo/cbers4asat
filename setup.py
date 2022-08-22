655b74458eab22948978f4dd6d9d03ae14d2d194
Gabriel Russo <gabrielrusso@protonmail.com>
2022-08-22 15:40:24 -0400

    v0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
         name='cbers4asat',
-        version='0.5',
+        version='0.6',
         description='Biblioteca Python para consultar o catálogo e realizar operações com dados do CBERS4A',
         url='https://github.com/gabriel-russo/cbers4asat',
         author='Gabriel Russo',
@@ -15,8 +15,6 @@ setup(
         long_description=long_description,
         long_description_content_type="text/markdown",
         classifiers=[
-                "Programming Language :: Python :: 3.7",
-                "Programming Language :: Python :: 3.8",
                 "Programming Language :: Python :: 3.9",
                 "Programming Language :: Python :: 3.10",
                 "License :: OSI Approved :: MIT License",
