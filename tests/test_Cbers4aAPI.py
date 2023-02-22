154ddfa890740bb68f0037388189ec588140f991
Gabriel Russo <gabrielrusso@protonmail.com>
2023-02-22 18:40:47 -0400

    Testes: query com path row, to_geodataframe e download

--- tests/test_Cbers4aAPI.py
+++ tests/test_Cbers4aAPI.py
@@ -1,14 +1,19 @@
 from cbers4asat import Cbers4aAPI
 import pytest
 from datetime import date
+from os import remove
 
 
-class MockStacfeatureCollectionResponse:
+class MockStacFeatureCollectionResponse:
     def __init__(self):
         self.status_code = 200
 
     def raise_for_status(self):
         pass
+
+    @staticmethod
+    def iter_content(chunk_size):
+        yield b"dummydata"
 
     @staticmethod
     def json():
@@ -19,17 +24,28 @@ class MockStacfeatureCollectionResponse:
                     "type": "Feature",
                     "id": "ABC123",
                     "collection": "y",
-                    "geometry": {"type": "Polygon", "coordinates": []},
-                    "bbox": [],
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [-48.3106, -15.3637],
+                                [-48.3106, -16.4178],
+                                [-47.2492, -16.4178],
+                                [-47.2492, -15.3637],
+                                [-48.3106, -15.3637],
+                            ]
+                        ],
+                    },
+                    "bbox": [-48.3106, -16.4178, -47.2492, -15.3637],
                     "properties": {"x": "XYZ"},
-                    "assets": {"band": {"x": "y"}},
+                    "assets": {"blue": {"href": "http://test.dev/image.tif"}},
                     "links": [{"x": "y"}],
                 }
             ],
         }
 
 
-class MockStacfeatureResponse:
+class MockStacFeatureResponse:
     def __init__(self):
         self.status_code = 200
 
@@ -37,15 +53,30 @@ class MockStacfeatureResponse:
         pass
 
     @staticmethod
+    def iter_content(chunk_size):
+        yield b"dummydata"
+
+    @staticmethod
     def json():
         return {
             "type": "Feature",
             "id": "ABC123",
             "collection": "y",
-            "geometry": {"type": "Polygon", "coordinates": []},
-            "bbox": [],
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [-48.3106, -15.3637],
+                        [-48.3106, -16.4178],
+                        [-47.2492, -16.4178],
+                        [-47.2492, -15.3637],
+                        [-48.3106, -15.3637],
+                    ]
+                ],
+            },
+            "bbox": [-48.3106, -16.4178, -47.2492, -15.3637],
             "properties": {"x": "XYZ"},
-            "assets": {"band": {"x": "y"}},
+            "assets": {"blue": {"href": "http://test.dev/image.tif"}},
             "links": [{"x": "y"}],
         }
 
@@ -59,10 +90,21 @@ class TestCbers4aAPI:
                 "type": "Feature",
                 "id": "ABC123",
                 "collection": "y",
-                "geometry": {"type": "Polygon", "coordinates": []},
-                "bbox": [],
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [-48.3106, -15.3637],
+                            [-48.3106, -16.4178],
+                            [-47.2492, -16.4178],
+                            [-47.2492, -15.3637],
+                            [-48.3106, -15.3637],
+                        ]
+                    ],
+                },
+                "bbox": [-48.3106, -16.4178, -47.2492, -15.3637],
                 "properties": {"x": "XYZ"},
-                "assets": {"band": {"x": "y"}},
+                "assets": {"blue": {"href": "http://test.dev/image.tif"}},
             }
         ],
     }
@@ -72,13 +114,11 @@ class TestCbers4aAPI:
 
     def test_set_user(self):
         self.api.user = "another@test.com"
-
         assert self.api.user == "another@test.com"
 
-    @pytest.fixture(autouse=True)
-    def test_query(self, monkeypatch):
+    def test_query_bbox(self, monkeypatch):
         def mock_post(*args, **kwargs):
-            return MockStacfeatureCollectionResponse()
+            return MockStacFeatureCollectionResponse()
 
         monkeypatch.setattr("requests.Session.post", mock_post)
 
@@ -93,10 +133,26 @@ class TestCbers4aAPI:
 
         assert self.expected_result == result
 
-    @pytest.fixture(autouse=True)
+    def test_query_pathrow(self, monkeypatch):
+        def mock_post(*args, **kwargs):
+            return MockStacFeatureCollectionResponse()
+
+        monkeypatch.setattr("requests.Session.post", mock_post)
+
+        result = self.api.query(
+            location=(206, 133),
+            initial_date=date(2021, 1, 1),
+            end_date=date(2021, 2, 1),
+            cloud=100,
+            limit=1,
+            collections=["CBERS4A_WPM_L4_DN"],
+        )
+
+        assert self.expected_result == result
+
     def test_query_by_id_str(self, monkeypatch):
         def mock_get(*args, **kwargs):
-            return MockStacfeatureResponse()
+            return MockStacFeatureResponse()
 
         monkeypatch.setattr("requests.Session.get", mock_get)
 
@@ -104,13 +160,58 @@ class TestCbers4aAPI:
 
         assert self.expected_result == result
 
-    @pytest.fixture(autouse=True)
     def test_query_by_id_list(self, monkeypatch):
         def mock_get(*args, **kwargs):
-            return MockStacfeatureResponse()
+            return MockStacFeatureResponse()
 
         monkeypatch.setattr("requests.Session.get", mock_get)
 
         result = self.api.query_by_id(["ABC123"])
 
         assert self.expected_result == result
+
+    def test_to_geodataframe(self):
+        gdf = self.api.to_geodataframe(self.expected_result)
+        assert type(gdf).__name__ == "GeoDataFrame"
+        assert gdf.crs == "EPSG:4326"
+        assert len(gdf) == 1
+
+    def test_download(self, monkeypatch, tmp_path):
+        def mock_get(*args, **kwargs):
+            return MockStacFeatureCollectionResponse()
+
+        monkeypatch.setattr("requests.Session.get", mock_get)
+
+        self.api.download(
+            products=self.expected_result,
+            bands=["blue"],
+            threads=1,
+            outdir=tmp_path.as_posix(),
+            with_folder=True,
+        )
+
+        with open(f"{tmp_path.as_posix()}/ABC123/image.tif", "rb") as f:
+            assert f.read() == b"dummydata"
+
+        remove(f"{tmp_path.as_posix()}/ABC123/image.tif")
+
+    def test_download_gdf(self, monkeypatch, tmp_path):
+        def mock_get(*args, **kwargs):
+            return MockStacFeatureResponse()
+
+        monkeypatch.setattr("requests.Session.get", mock_get)
+
+        gdf = self.api.to_geodataframe(self.expected_result)
+
+        self.api.download(
+            products=gdf,
+            bands=["blue"],
+            threads=1,
+            outdir=tmp_path.as_posix(),
+            with_folder=True,
+        )
+
+        with open(f"{tmp_path.as_posix()}/ABC123/image.tif", "rb") as f:
+            assert f.read() == b"dummydata"
+
+        remove(f"{tmp_path.as_posix()}/ABC123/image.tif")
