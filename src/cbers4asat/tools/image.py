280ce680f52242af9f98ec86e66fe3e1d10cb5a4
Gabriel Russo <gabrielrusso@protonmail.com>
2023-03-23 18:45:57 -0400

    Otimizacao: Otimização no algoritmo rgbn_composite

--- src/cbers4asat/tools/image.py
+++ src/cbers4asat/tools/image.py
@@ -1,14 +1,14 @@
 from rasterio import open as rasterio_open
-from os import getcwd
-from os.path import isfile, join
-from numpy import stack, squeeze
+from os import getcwd, makedirs
+from os.path import isfile, join, exists
+from numpy import stack
 
 
 def rgbn_composite(
     red: str,
     green: str,
     blue: str,
-    nir=None,
+    nir: str = None,
     outdir: str = getcwd(),
     filename: str = "rgbn_composite.tif",
 ):
@@ -37,9 +37,12 @@ def rgbn_composite(
         elif nir is not None and not isfile(nir):
             raise FileNotFoundError("Check band's file path")
 
+        if not exists(outdir):
+            makedirs(outdir)
+
         bands_metadata = bands[0].meta.copy()
 
-        merged = stack([squeeze(band.read(), axis=0) for band in bands])
+        merged = stack([band.read(1) for band in bands])
 
         for band in bands:
             band.close()
